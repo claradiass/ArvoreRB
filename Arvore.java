98f224d4e0eb203d87a67ac7e427c7e3eb54ef01
claradiass <claragoncalves370@gmail.com>
2024-09-17 08:44:49 -0300

    Rotações

--- Arvore.java
+++ Arvore.java
@@ -47,66 +47,137 @@ public class Arvore {
 
 
     public void cor(No no){
-        No pai = no.getPai();
-        if(no.getPai() == null){
-            no.setEhVermelho(false);  // Garante que a raiz sempre seja preta
-            return;
-        }
+        while(no.getPai() != null && no.getPai().ehVermelho){ // enquanto o pai não for a raiz e ele estiver vermelho
 
-        if(!no.getPai().ehVermelho){
-            return;
-        }
+            if(no.getPai().getPai() != null && no.getPai() == no.getPai().getPai().getEsquerda()){ // se o avô não for nulo e se o pai for o filho a esquerda do avô, significa que o tio do nó está a direita
+                No tio = no.getPai().getPai().getDireita(); // o tio está à direita do avô
+                if (tio != null && tio.ehVermelho) { // Caso 1: o tio é difenrente de null e é vermelho
+                    no.getPai().setEhVermelho(false); // pai fica preto
+                    tio.setEhVermelho(false); // tio fica preto
+                    no.getPai().getPai().setEhVermelho(true); // avô fica vermelho
+                    no = no.getPai().getPai(); // sobe o problema para o avô
+                } else { // tio preto ou nulo
+                    rotacionar(no);
 
 
-        No tio = null;
 
 
-        if(no.getDado() < pai.getDado()){
-            if(pai.getPai().getDireita() != null){
-                tio = pai.getPai().getDireita();
-                pai.setEhVermelho(false);
-                tio.setEhVermelho(false);
+                }
+            } else {
+                No tio = no.getPai().getPai().getEsquerda();
+                if (tio != null && tio.ehVermelho) { // Caso 1: o tio é difenrente de null e é vermelho
+                    no.getPai().setEhVermelho(false); // pai fica preto
+                    tio.setEhVermelho(false); // tio fica preto
+                    no.getPai().getPai().setEhVermelho(true); // avô fica vermelho
+                    no = no.getPai().getPai(); // sobe o problema para o avô
+                } else { // tio preto ou nulo
+                    if(no == no.getPai().getDireita()) { // no filho a direita
+                        rotacaoDireita(no.getPai());
+                    }
+                    // no é flho a esquerda
+                    no.getPai().setEhVermelho(false); // pai fica preto
+                    no.getPai().getPai().setEhVermelho(true); // avô fica vermelho
+                    rotacaoEsquerda(no.getPai().getPai()); // rotação a direita do avô
+                }
             }
-
-        } else if (no.getDado() > pai.getDado()){
-            if(pai.getPai().getEsquerda() != null){
-                tio = pai.getPai().getEsquerda();
-                pai.setEhVermelho(false);
-                tio.setEhVermelho(false);
-            }
         }
+        raiz.setEhVermelho(false);
 
-
-
-
-
-
-        if(pai.ehVermelho && tio.ehVermelho){
-            pai.setEhVermelho(false);
-            tio.setEhVermelho(false);
-        }
-
-
-
-
-        cor(pai);
-    }
-
-
-    public int alturaPreta(No no){
-        if (no.getEsquerda() == null){
-            return 1;
-
-
-        } else if (no.getDireita() == null){
-            return 1;
-        }
-
-
-        return alturaPreta(no.getDireita()) - alturaPreta(no.getEsquerda());
 
 
     }
+
+    public void rotacionar(No no){
+        if(no == no.getPai().getDireita()) { // no filho a direita
+            rotacaoEsquerda(no.getPai());
+
+
+        } else if(no == no.getPai().getEsquerda()) {
+            // no é flho a esquerda
+            no.getPai().setEhVermelho(false); // pai fica preto
+            no.getPai().getPai().setEhVermelho(true); // avô fica vermelho
+            rotacaoDireita(no.getPai().getPai()); // rotação a direita do avô
+        } // Se o nó está na subárvore direita e o pai está na subárvore esquerda
+        else if (no == no.getPai().getDireita() && no.getPai() == no.getPai().getPai().getEsquerda()) {
+            // Primeira rotação à esquerda no pai
+            rotacaoEsquerda(no.getPai());
+            // Depois, rotação à direita no avô
+            rotacaoDireita(no.getPai().getPai());
+        }
+// Se o nó está na subárvore esquerda e o pai está na subárvore direita
+        else if (no == no.getPai().getEsquerda() && no.getPai() == no.getPai().getPai().getDireita()) {
+            // Primeira rotação à direita no pai
+            rotacaoDireita(no.getPai());
+            // Depois, rotação à esquerda no avô
+            rotacaoEsquerda(no.getPai().getPai());
+        }
+
+
+
+
+
+
+
+
+//    } else if(no == no.getPai().getDireita() && no.getPai().getPai().getEsquerda() == no.getPai()){
+//        rotacaoEsquerda(no.getPai());
+//        rotacaoDireita(no.getPai().getPai());
+//    } else if(no == no.getPai().getEsquerda() && no.getPai().getPai().getDireita() == no.getPai()){
+//        rotacaoDireita(no.getPai());
+//        rotacaoEsquerda(no.getPai().getPai());
+//    }
+
+    }
+
+
+
+
+
+
+
+
+    public void rotacaoDireita(No avo){
+
+        if(avo.getEsquerda() == null) {
+            return; // Não há nada para rotacionar
+        }
+
+        avo.getEsquerda().setPai(avo.getPai());
+        if(avo.getPai() != null){
+            avo.getPai().setEsquerda(avo.getEsquerda());
+        } else {
+            raiz = avo.getEsquerda();
+        }
+        avo.setPai(avo.getEsquerda());
+        avo.setEsquerda( avo.getEsquerda().getDireita());
+
+        if(avo.getEsquerda() != null){
+            avo.getEsquerda().setPai(avo);
+        }
+        avo.getPai().setDireita(avo);
+    }
+
+    public void rotacaoEsquerda(No avo){
+        if(avo.getDireita() == null) {
+            return; // Não há nada para rotacionar
+        }
+        avo.getDireita().setPai(avo.getPai());
+        if(avo.getPai() != null){
+            avo.getPai().setDireita(avo.getDireita());
+        } else {
+            raiz = avo.getDireita();
+        }
+        avo.setPai(avo.getDireita());
+        avo.setDireita( avo.getDireita().getEsquerda());
+
+        if(avo.getDireita() != null){
+            avo.getDireita().setPai(avo);
+        }
+        avo.getPai().setEsquerda(avo);
+    }
+
+
+
 
 
 
--- Main.java
+++ Main.java
@@ -6,23 +6,30 @@ public class Main {
         a.inserir(40);
         a.inserir(60);
         a.inserir(30);
-        a.inserir(45);
         a.inserir(29);
-//        a.inserir(23);
+        a.inserir(23);
+        a.inserir(65);
+        a.inserir(68);
+
+        a.inserir(24);
 
         System.out.println(a.raiz.getDado());
-        System.out.println(a.raiz.ehVermelho);
+        System.out.println(a.raiz.ehVermelho); // 50
         System.out.println(a.raiz.getEsquerda().getDado());
-        System.out.println(a.raiz.getEsquerda().ehVermelho);
+        System.out.println(a.raiz.getEsquerda().ehVermelho); // 30 v
         System.out.println(a.raiz.getDireita().getDado());
-        System.out.println(a.raiz.getDireita().ehVermelho);
+        System.out.println(a.raiz.getDireita().ehVermelho); // 65 p
         System.out.println(a.raiz.getEsquerda().getEsquerda().getDado());
-        System.out.println(a.raiz.getEsquerda().getEsquerda().ehVermelho);
+        System.out.println(a.raiz.getEsquerda().getEsquerda().ehVermelho); // 29 p
         System.out.println(a.raiz.getEsquerda().getDireita().getDado());
-        System.out.println(a.raiz.getEsquerda().getDireita().ehVermelho);
+        System.out.println(a.raiz.getEsquerda().getDireita().ehVermelho); // 40 p
         System.out.println(a.raiz.getEsquerda().getEsquerda().getEsquerda().getDado());
-        System.out.println(a.raiz.getEsquerda().getEsquerda().getEsquerda().ehVermelho);
-        System.out.println(a.raiz.getEsquerda().getEsquerda().getEsquerda().getEsquerda().getDado());
-        System.out.println(a.raiz.getEsquerda().getEsquerda().getEsquerda().getEsquerda().ehVermelho);
+        System.out.println(a.raiz.getEsquerda().getEsquerda().getEsquerda().ehVermelho); // 23 v
+//        System.out.println(a.raiz.getEsquerda().getEsquerda().getEsquerda().getDireita().getDado());
+//        System.out.println(a.raiz.getEsquerda().getEsquerda().getEsquerda().getDireita().ehVermelho);
+        System.out.println(a.raiz.getDireita().getEsquerda().getDado());
+        System.out.println(a.raiz.getDireita().getEsquerda().ehVermelho); // 60 v
+        System.out.println(a.raiz.getDireita().getDireita().getDado());
+        System.out.println(a.raiz.getDireita().getDireita().ehVermelho); // 68 v
     }
 }
